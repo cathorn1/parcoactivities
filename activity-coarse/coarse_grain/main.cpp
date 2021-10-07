b2bc7127c1b7442ea3d543044e7c5fbed41ba7de
CliftonT <cathorn1@uncc.edu>
2021-10-07 19:53:24 -0400

    ohboy

--- activity-coarse/coarse_grain/main.cpp
+++ activity-coarse/coarse_grain/main.cpp
@@ -48,9 +48,9 @@ std::vector<std::vector<std::string>> tokenizeLyrics(const std::vector<std::stri
   return ret;
 }
 
- void countWords(std::string filecontent, Dictionary<std::string, int> dict, std::mutex& mut){
-    for (auto &p : filecontent) {
-        std::string w = *p;
+ void countWords(std::string &filecontent, Dictionary<std::string, int> dict, std::mutex& mut){
+    for (auto &w : filecontent) {
+        
         int count = dict.get(w);
         mut.lock();
         ++count;
@@ -122,9 +122,9 @@ int main(int argc, char **argv)
   std::vector<std::thread> countedThreads;
   std::mutex mu;
 
-  for (auto & filecontent : wordmap) {
-      auto content = *filecontent;
-      std::thread hashThread (countWords, content, std::ref(dict), std::ref(mu));
+  for (auto &filecontent : wordmap) {
+      //auto content = filecontent;
+      std::thread hashThread (countWords, std::ref(filecontent), std::ref(dict), std::ref(mu));
 
       countedThreads.push_back(move(hashThread));      
     }
