4fc55e6b4436bbc121db9f7c3dac9f78299ea20c
Clifton Andrew Thorne <cathorn1@gal-i1.uncc.edu>
2021-10-07 21:04:20 -0400

    t

--- activity-coarse/coarse_grain/main.cpp
+++ activity-coarse/coarse_grain/main.cpp
@@ -48,7 +48,7 @@ std::vector<std::vector<std::string>> tokenizeLyrics(const std::vector<std::stri
   return ret;
 }
 
- void countWords(char* &filecontent, Dictionary<std::string, int> &dict, std::mutex& mut){
+ void countWords(std::string &filecontent, Dictionary<std::string, int> &dict, std::mutex& mut){
     for (auto &w : filecontent) {
         
         int count = dict.get(w);
@@ -106,8 +106,8 @@ int main(int argc, char **argv)
   std::mutex mu;
 
   for (auto & filecontent : wordmap) {
-      //auto content = filecontent;
-      std::thread hashThread (countWords, std::ref(filecontent), std::ref(dict), std::ref(mu));
+      auto content = filecontent;
+      std::thread hashThread (countWords, std::ref(content), std::ref(dict), std::ref(mu));
 
       countedThreads.push_back(move(hashThread));      
     }
