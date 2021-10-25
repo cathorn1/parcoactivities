557a4679cf404755a3ec061ead4fbcc986814f75
CliftonT <cathorn1@uncc.edu>
2021-10-25 18:47:58 -0400

    D

--- activity-parloop-static/sequential/seq_loop.hpp
+++ activity-parloop-static/sequential/seq_loop.hpp
@@ -120,7 +120,7 @@ void parfor (size_t beg, size_t end, size_t increment, size_t n, size_t gran,
     int chunkRemain = n % gran;
     std::vector <std::thread> tVec;
     int up, low;
-    printf("chunk top: %d, up: %d, low: %d", chunkSize, up, low);
+    //printf("chunk top: %d, up: %d, low: %d", chunkSize, up, low);
     //std::cout << "chunk: " << chunkSize << "up: " << up << "low: " << low << "/n";
 
     for(size_t i = beg; i < end; i+=chunkSize){
@@ -137,7 +137,7 @@ void parfor (size_t beg, size_t end, size_t increment, size_t n, size_t gran,
 
         t.join();
 
-        printf("chunk: %d, up: %d, low: %d", chunkSize, up, low);
+       // printf("chunk: %d, up: %d, low: %d", chunkSize, up, low);
         //std::cout << "chunk: " << chunkSize << "up: " << up << "low: " << low << "\n";
         inc++;
         counter++;
