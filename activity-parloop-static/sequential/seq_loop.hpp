82c3cef516fa9ed68ef4188ca6e8b99386086123
CliftonT <cathorn1@uncc.edu>
2021-10-25 13:26:25 -0400

    D

--- activity-parloop-static/sequential/seq_loop.hpp
+++ activity-parloop-static/sequential/seq_loop.hpp
@@ -118,7 +118,7 @@ void parfor (size_t beg, size_t end, size_t increment, size_t n, size_t gran,
     int chunkRemain = n % gran;
     std::vector <std::thread> tVec;
 
-    while (counter < chunkSize) {
+    while (counter < itrs) {
         int up = chunkSize * inc;
         int low = up - chunkSize;
         up -= 1;
@@ -135,7 +135,7 @@ void parfor (size_t beg, size_t end, size_t increment, size_t n, size_t gran,
     for (auto &t: tVec) {
         if (t.joinable())
             t.join();
-        else
+        //else
         // t = std::thread(f, low, up, std::ref(tls));
         //std::cout << "t is not joinable" << std::endl;
     }
