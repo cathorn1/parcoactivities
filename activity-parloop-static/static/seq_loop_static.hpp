ef362dfae7d1bc6794f7816256f1003d227f8989
CliftonT <cathorn1@uncc.edu>
2021-10-28 19:30:12 -0400

    d

--- activity-parloop-static/static/seq_loop_static.hpp
+++ activity-parloop-static/static/seq_loop_static.hpp
@@ -124,7 +124,7 @@ void parfor (size_t beg, size_t end, size_t increment, size_t n,
                up += remain;
            }
 
-           printf("low: %d, up: %d, inc: %d, itrs: %d, remain: %d \n", low, up, inc, itrs, remain);
+           //printf("low: %d, up: %d, inc: %d, itrs: %d, remain: %d \n", low, up, inc, itrs, remain);
 
            tVec.push_back(std::thread(f, low, up, std::ref(tls)));
 
