b41735efa9ff08f311efa30cf90eafc13b8aabd2
CliftonT <cathorn1@uncc.edu>
2021-10-30 18:18:59 -0400

    d

--- activity-lcs/lcs/lcs.cpp
+++ activity-lcs/lcs/lcs.cpp
@@ -51,10 +51,10 @@ int main (int argc, char* argv[]) {
                     //tls[i][0] = 0;
                     //printf("%s\n", "howdy 1");
                 }
-                for (int j = 0; j <= n; ++j) {
-                    //printf("%s\n", "howdy 2");
-                    tls[0][j] = 0;
-                }
+//                for (int j = 0; j <= n; ++j) {
+//                    //printf("%s\n", "howdy 2");
+//                    tls[0][j] = 0;
+//                }
             },
             [&](int a, int b, char* U, char* W, std::vector <std::vector<int>> &tls) -> void {
                 //printf("%s\n", "howdy 3");
