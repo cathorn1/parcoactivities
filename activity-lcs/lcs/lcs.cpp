60b5840d818c494287dbacca3e1f10e670b8665d
CliftonT <cathorn1@uncc.edu>
2021-10-30 19:05:13 -0400

    d

--- activity-lcs/lcs/lcs.cpp
+++ activity-lcs/lcs/lcs.cpp
@@ -83,16 +83,16 @@ int main (int argc, char* argv[]) {
 
 
 
-    int result = answer; // length of common subsequence
-    printf("%s", "made bottom");
-    printf("%s %d\n", "from end", result);
-
-    std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
-    std::chrono::duration<double> elpased_seconds = end-start;
-
-    checkLCS(X, m, Y, n, result);
-
-    std::cerr<<elpased_seconds.count()<<std::endl;
+//    int result = answer; // length of common subsequence
+//    printf("%s", "made bottom");
+//    printf("%s %d\n", "from end", result);
+//
+//    std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
+//    std::chrono::duration<double> elpased_seconds = end-start;
+//
+//    checkLCS(X, m, Y, n, result);
+//
+//    std::cerr<<elpased_seconds.count()<<std::endl;
 
     return 0;
 }
