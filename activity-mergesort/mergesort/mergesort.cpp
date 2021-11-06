72185b892bda5dafc6a3e19bab1f0b9e1df8dda0
CliftonT <cathorn1@uncc.edu>
2021-11-06 17:44:10 -0400

    d

--- activity-mergesort/mergesort/mergesort.cpp
+++ activity-mergesort/mergesort/mergesort.cpp
@@ -116,10 +116,10 @@ int main (int argc, char* argv[]) {
   int* arr = new int [n];
   generateMergeSortData (arr, n);
 
-//    for (int i =0; i < n; i++) {
-//        std::cout << arr[i] << " ";
-//    }
-//    std::cout << "\n";
+    for (int i =0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << "\n";
 
 
     #if DEBUG
@@ -166,11 +166,11 @@ int main (int argc, char* argv[]) {
                 }
 //
 
-//                std::cout << "middle test\n";
-//                for (int i =0; i < n; i++) {
-//                    std::cout << arr[i] << " ";
-//                }
-//                std::cout << "\n";
+                std::cout << "middle test\n";
+                for (int i =0; i < n; i++) {
+                    std::cout << arr[i] << " ";
+                }
+                std::cout << "\n";
 
             },
             [&](std::vector<int> &C) -> void {
@@ -179,11 +179,11 @@ int main (int argc, char* argv[]) {
 //                    arr[i] = C[i];
 //                }
 
-//                       std::cout << "test C\n";
-//                       for (int i =0; i < n; i++) {
-//                           std::cout << arr[i] << " ";
-//                       }
-//                       std::cout << "\n";
+                       std::cout << "test C\n";
+                       for (int i =0; i < n; i++) {
+                           std::cout << arr[i] << " ";
+                       }
+                       std::cout << "\n";
             });
 
     // end timing
