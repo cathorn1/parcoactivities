164439b0c3f69aa3bcee0c6956a5c200e97b99b6
CliftonT <cathorn1@uncc.edu>
2021-11-06 14:32:51 -0400

    d

--- activity-mergesort/mergesort/mergesort.cpp
+++ activity-mergesort/mergesort/mergesort.cpp
@@ -128,7 +128,7 @@ int main (int argc, char* argv[]) {
     // begin timing
     std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
 
-    omp.parfor<std::vector<int>>(0, n, 1,
+    omp.parfor<std::vector<int>>(0, n/nbthreads, 1,
             [&](std::vector<int> &C) -> void {
                 for(int i = 0; i < n; i++){
                     //std::cout << "p1\n";
