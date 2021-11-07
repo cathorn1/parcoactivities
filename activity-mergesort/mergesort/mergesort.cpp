f283008471856adc00494d06bf6549e972bc81e8
CliftonT <cathorn1@uncc.edu>
2021-11-07 10:34:49 -0500

    d

--- activity-mergesort/mergesort/mergesort.cpp
+++ activity-mergesort/mergesort/mergesort.cpp
@@ -276,10 +276,10 @@ int main (int argc, char* argv[]) {
                        // for (left_start = 0; left_start < n - 1; left_start += 2 * curr_size) {
                             // Find ending point of left subarray. mid+1 is starting
                             // point of right
-                            left_start += 2*curr_size;
-                            int mid = std::min(left_start + curr_size - 1, n - 1);
+                            left_start += chunk;
+                            int mid = std::min(left_start + chunk - 1, n - 1);
 
-                            int right_end = std::min(left_start + 2 * curr_size - 1, n - 1);
+                            int right_end = std::min(left_start + 2 * chunk - 1, n - 1);
 
                             // Merge Subarrays arr[left_start...mid] & arr[mid+1...right_end]
                             //std::lock_guard <std::mutex> lck(mut);
