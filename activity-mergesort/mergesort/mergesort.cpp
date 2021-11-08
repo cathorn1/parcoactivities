aeb057eeb6b15b24d1cf2b19ed1475740cb92494
CliftonT <cathorn1@uncc.edu>
2021-11-08 10:21:15 -0500

    d

--- activity-mergesort/mergesort/mergesort.cpp
+++ activity-mergesort/mergesort/mergesort.cpp
@@ -110,10 +110,10 @@ int main (int argc, char* argv[]) {
   int* arr = new int [n];
   generateMergeSortData (arr, n);
 
-    for (int i =0; i < n; i++) {
-        std::cout << arr[i] << " ";
-    }
-    std::cout << "\n";
+//    for (int i =0; i < n; i++) {
+//        std::cout << arr[i] << " ";
+//    }
+//    std::cout << "\n";
 
 
     #if DEBUG
@@ -154,11 +154,11 @@ int main (int argc, char* argv[]) {
 //                        }
 //                    }
 
-                    std::cout << "middle test\n";
-                    for (int i = 0; i < n; i++) {
-                        std::cout << arr[i] << " ";
-                    }
-                    std::cout << "\n";
+//                    std::cout << "middle test\n";
+//                    for (int i = 0; i < n; i++) {
+//                        std::cout << arr[i] << " ";
+//                    }
+//                    std::cout << "\n";
 
                 },
                 [&](std::vector<int> &C) -> void {
