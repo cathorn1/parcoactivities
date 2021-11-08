af839402de25915860a52e1b9f4fd7048cf23dcb
CliftonT <cathorn1@uncc.edu>
2021-11-08 15:56:51 -0500

    d

--- activity-mergesort/mergesort/mergesort.cpp
+++ activity-mergesort/mergesort/mergesort.cpp
@@ -67,7 +67,9 @@ void merge(int arr[], int temp[], int l, int m, int r) {
     }
 
     for(int k = 0; k < (r-l); k++){
-        arr[k] = temp[k];
+       mut.lock();
+       arr[k] = temp[k];
+       mut.unlock();
     }
 }
 
