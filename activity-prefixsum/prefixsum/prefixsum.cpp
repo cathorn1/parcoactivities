cd17178114de247b8ab6d86173bb47a2847015ad
CliftonT <cathorn1@uncc.edu>
2021-11-08 21:43:32 -0500

    m

--- activity-prefixsum/prefixsum/prefixsum.cpp
+++ activity-prefixsum/prefixsum/prefixsum.cpp
@@ -65,7 +65,7 @@ int main (int argc, char* argv[]) {
 
     //int chunk = pow(2,nbthreads +1);
 
-    for (int i = 0; i <= (log2(n)); i++) {
+    for (int i = 0; i <= (log(n)-1); i++) {
 
         omp.parfor<std::vector<int>>(0, n-1, pow(2, i+1),
                 [&](std::vector<int> &C) -> void {
@@ -84,7 +84,7 @@ int main (int argc, char* argv[]) {
                 });
    }
 
-    for (int i = (log2(n)); i >= 0 ; i--) {
+    for (int i = (log(n)-1); i >= 0 ; i--) {
 
         omp.parfor<std::vector<int>>(0, n-1, pow(2, i+1),
                 [&](std::vector<int> &C) -> void {
@@ -103,7 +103,7 @@ int main (int argc, char* argv[]) {
                 });
     }
 
-
+arr[n-1] = 0;
     std::cout << "\nprint arr" << std::endl;
     for (int i = 0; i < n + 1; ++i) {
         std::cout << arr[i] << " ";
