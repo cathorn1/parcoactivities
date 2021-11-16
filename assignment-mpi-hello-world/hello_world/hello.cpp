b14927df9ada6faa51e794c243697b0f85f8fce0
CliftonT <cathorn1@uncc.edu>
2021-11-15 19:12:41 -0500

    d

--- assignment-mpi-hello-world/hello_world/hello.cpp
+++ assignment-mpi-hello-world/hello_world/hello.cpp
@@ -1,6 +1,7 @@
 #include <mpi.h>
 #include <unistd.h>
 #include <iostream>
+#include <chrono>
 
 int main(int argc, char*argv[]) {
     MPI_Init (&argc, &argv);
@@ -11,8 +12,17 @@ int main(int argc, char*argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    std::cout << "I am proccess " << rank << "out of " << size
+    std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
+
+
+    std::cout << "I am proccess " << rank << " out of " << size
     << ". I am running on machine: " << getpid() << std::endl;
+
+    std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elpased_seconds = end-start;
+
+    // display time to cerr
+    std::cerr<<elpased_seconds.count()<<std::endl;
 
     MPI_Finalize();
 }
