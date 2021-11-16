2ef0596268418db89205b2971eff2f15ef2fed0a
CliftonT <cathorn1@uncc.edu>
2021-11-15 19:10:09 -0500

    d

--- assignment-mpi-hello-world/hello_world/hello.cpp
+++ assignment-mpi-hello-world/hello_world/hello.cpp
@@ -5,6 +5,9 @@
 int main(int argc, char*argv[]) {
     MPI_Init (&argc, &argv);
 
+    int size;
+    int rank;
+
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
