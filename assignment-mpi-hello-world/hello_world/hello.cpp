ce1c80dd4231a74cc9d92b380fe4283a00d02f69
CliftonT <cathorn1@uncc.edu>
2021-11-16 17:25:34 -0500

    s

--- assignment-mpi-hello-world/hello_world/hello.cpp
+++ assignment-mpi-hello-world/hello_world/hello.cpp
@@ -10,7 +10,6 @@ int main(int argc, char*argv[]) {
 
     int size;
     int rank;
-
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     int pid = getpid();
--- assignment-mpi-numint-collective/num_int/mpi_num_int.cpp
+++ assignment-mpi-numint-collective/num_int/mpi_num_int.cpp
@@ -17,7 +17,38 @@ float f4(float x, int intensity);
 }
 #endif
 
-  
+double integrateNum (int func, int points, double lower, double upper, int intensity, int begin, int end) {
+
+    double itgr_output = 0.0;
+    double x = 0.0;
+
+    for (int i = begin; i < end; i++) {
+
+        if (func == 1) {
+            for (int i = 0; i <= (points - 1); i++) {
+                x = ((lower + (i + .5)) * ((upper - lower) / points));
+                itgr_output += ((upper - lower) / points) * f1(x, intensity);
+            }
+        } else if (func == 2) {
+            for (int i = 0; i <= (points - 1); i++) {
+                x = ((lower + (i + .5)) * ((upper - lower) / points));
+                itgr_output += ((upper - lower) / points) * f2(x, intensity);
+            }
+        } else if (func == 3) {
+            for (int i = 0; i <= (points - 1); i++) {
+                x = ((lower + (i + .5)) * ((upper - lower) / points));
+                itgr_output += ((upper - lower) / points) * f3(x, intensity);
+            }
+        } else if (func == 4) {
+            for (int i = 0; i <= (points - 1); i++) {
+                x = ((lower + (i + .5)) * ((upper - lower) / points));
+                itgr_output += ((upper - lower) / points) * f4(x, intensity);
+            }
+        }
+    }
+    return itgr_output;
+}
+
 int main (int argc, char* argv[]) {
   
   if (argc < 6) {
@@ -25,6 +56,12 @@ int main (int argc, char* argv[]) {
     return -1;
   }
     MPI_Init (&argc, &argv);
+
+    int func = atoi(argv[1]);
+    double lower = atof(argv[2]);
+    double upper = atof(argv[3]);
+    int points = atoi(argv[4]);
+    int intensity = atoi(argv[5]);
 
     std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
 
@@ -34,9 +71,26 @@ int main (int argc, char* argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+    //int chunk = points/size;
 
+    //MPI process should take N/P iterations of the loop
+    // and accumulate on rank 0
+    int begin = rank*(points/size);
+    int end = (rank+1)*(points/size);
+    double * integral;
+    integral = integrateNum(func, point, lower, upper, intensity, begin, end);
 
-
+    if(rank != 0) {
+        //send integral to rank 0
+        MPI_send(integral, 1, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD);
+    }
+    else {
+        for (int i = 1; i < size; i++){
+            //receive integralp from i
+            //integral += integralp
+            MPI_receive(integral, 1, MPI_DOUBLE, i, MPI_ANY_TAG, MPI_COMM_WORLD);
+        }
+    }
 
 
     std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
@@ -44,6 +98,7 @@ int main (int argc, char* argv[]) {
 
     // display time to cerr
     std::cerr<<elpased_seconds.count()<<std::endl;
+    std::cout << result <<std::endl;
 
     MPI_Finalize();
 
