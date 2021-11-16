5c3c5e982a2af01b4f6db5105a3e22c5f85f277f
CliftonT <cathorn1@uncc.edu>
2021-11-15 19:53:52 -0500

    d

--- assignment-mpi-numint-collective/num_int/mpi_num_int.cpp
+++ assignment-mpi-numint-collective/num_int/mpi_num_int.cpp
@@ -1,23 +1,43 @@
-#include <mpi.h>
-#include <unistd.h>
 #include <iostream>
+#include <cmath>
+#include <cstdlib>
 #include <chrono>
+#include <mpi.h>
 
-int main(int argc, char*argv[]) {
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+float f1(float x, int intensity);
+float f2(float x, int intensity);
+float f3(float x, int intensity);
+float f4(float x, int intensity);
+
+#ifdef __cplusplus
+}
+#endif
+
+  
+int main (int argc, char* argv[]) {
+  
+  if (argc < 6) {
+    std::cerr<<"usage: "<<argv[0]<<" <functionid> <a> <b> <n> <intensity>"<<std::endl;
+    return -1;
+  }
     MPI_Init (&argc, &argv);
 
     std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
 
     int size;
     int rank;
-    char hostname[64];
-    int name = gethostname(hostname, 64);
 
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    std::cout << "I am proccess " << rank << " out of " << size
-    << ". I am process #" << getpid << "running on machine: " << name << std::endl;
+
+
+
+
 
     std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
     std::chrono::duration<double> elpased_seconds = end-start;
@@ -26,4 +46,7 @@ int main(int argc, char*argv[]) {
     std::cerr<<elpased_seconds.count()<<std::endl;
 
     MPI_Finalize();
+
+
+  return 0;
 }
