f5099afc13f3b7f57fbe0b07c59d0da09616f042
CliftonT <cathorn1@uncc.edu>
2021-12-06 11:38:10 -0500

    init

--- activity-hello 3/pingpong/mpi_ping_pong.cpp
+++ activity-hello 3/pingpong/mpi_ping_pong.cpp
@@ -1,5 +1,6 @@
 #include <mpi.h>
 #include <iostream>
+#include <chrono>
 
 int main (int argc, char* argv[]) {
 
@@ -8,14 +9,43 @@ int main (int argc, char* argv[]) {
         return -1;
     }
 
+    MPI_Init (&argc, &argv);
+
     std::chrono::time_point<std::chrono::system_clock> time_start = std::chrono::system_clock::now();
 
+    int value = atoi(argv[0]);
+    int size;
+    int rank;
 
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (rank == 0) {
+        MPI_Send(&value, 1, MPI_INT, 1, 123, MPI_COMM_WORLD);
+    }
+    else if (rank == 1) {
+        MPI_Status s;
+        MPI_Recv(&value, 1, MPI_INT, 0, 123, MPI_COMM_WORLD, &s);
+        value += 2;
+        MPI_Send(&value, 1, MPI_INT, 0, 123, MPI_COMM_WORLD);
+    }
+
+    if (rank == 0) {
+        MPI_Recv(&value, 1, MPI_INT, 1, 123, MPI_COMM_WORLD, &s);
+        printf("%d", value);
+    }
+//    else if (rank == 1) {
+//        MPI_Status s;
+//        MPI_Recv(&value, 1, MPI_INT, 0, 123, MPI_COMM_WORLD, &s);
+//    }
 
     std::chrono::time_point<std::chrono::system_clock> time_end = std::chrono::system_clock::now();
     std::chrono::duration<double> elpased_seconds = time_end - time_start;
 
+    std::cout << value << std::endl;
     std::cerr << elpased_seconds.count() << std::endl;
+
+    MPI_Finalize();
 
     return 0;
 }
