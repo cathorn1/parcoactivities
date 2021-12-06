fb5a8cb5ba4638b14f151d4a3e63749603541024
CliftonT <cathorn1@uncc.edu>
2021-12-06 15:18:42 -0500

    init

--- activity-hello 3/pingpong/mpi_ping_pong.cpp
+++ activity-hello 3/pingpong/mpi_ping_pong.cpp
@@ -19,19 +19,19 @@ int main (int argc, char* argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     if (rank == 0) {
-        MPI_Send(&value, 1, MPI_INT, 1, 123, MPI_COMM_WORLD);
+        MPI_Send(&value, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
     }
     else if (rank == 1) {
-        MPI_Status s;
-        MPI_Recv(&value, 1, MPI_INT, 0, 123, MPI_COMM_WORLD, &s);
+//        MPI_Status s;
+        MPI_Recv(&value, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         value += 2;
-        MPI_Send(&value, 1, MPI_INT, 0, 123, MPI_COMM_WORLD);
+        MPI_Send(&value, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
     }
 
-//    if (rank == 0) {
-//        MPI_Recv(&value, 1, MPI_INT, 1, 123, MPI_COMM_WORLD, &s);
-//        printf("%d", value);
-//    }
+    if (rank == 0) {
+        MPI_Recv(&value, 1, MPI_INT, 1, 123, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("%d", value);
+    }
 //    else if (rank == 1) {
 //        MPI_Status s;
 //        MPI_Recv(&value, 1, MPI_INT, 0, 123, MPI_COMM_WORLD, &s);
