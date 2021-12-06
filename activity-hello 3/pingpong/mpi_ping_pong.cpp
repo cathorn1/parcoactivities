bd5d2ed66d6ef0519b9d585e2c7f94dc376dbca8
CliftonT <cathorn1@uncc.edu>
2021-12-06 15:56:14 -0500

    init

--- activity-hello 3/pingpong/mpi_ping_pong.cpp
+++ activity-hello 3/pingpong/mpi_ping_pong.cpp
@@ -22,10 +22,10 @@ int main (int argc, char* argv[]) {
 
     printf("value: %d", value);
 
-    if (rank == 0) {
-        MPI_Send(&value, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-        printf("pid 1: %d", pid);
-    }
+//    if (rank == 0) {
+//        MPI_Send(&value, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+//        printf("pid 1: %d", pid);
+//    }
 //    else if (rank == 1) {
 ////        MPI_Status s;
 //        MPI_Recv(&value, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
