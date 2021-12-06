dcc6efb29c6429cbd943711b0f58c313f9b2145b
CliftonT <cathorn1@uncc.edu>
2021-12-06 13:11:35 -0500

    init

--- activity-hello 3/pingpong/mpi_ping_pong.cpp
+++ activity-hello 3/pingpong/mpi_ping_pong.cpp
@@ -3,13 +3,12 @@
 #include <chrono>
 
 int main (int argc, char* argv[]) {
+    MPI_Init (&argc, &argv);
 
     if (argc < 2) {
         std::cerr<<"usage: mpirun "<<argv[0]<<" <value>"<<std::endl;
         return -1;
     }
-
-    MPI_Init (&argc, &argv);
 
     std::chrono::time_point<std::chrono::system_clock> time_start = std::chrono::system_clock::now();
 
@@ -19,12 +18,11 @@ int main (int argc, char* argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-
     if (rank == 0) {
         MPI_Send(&value, 1, MPI_INT, 1, 123, MPI_COMM_WORLD);
     }
     else if (rank == 1) {
-
+        MPI_Status s;
         MPI_Recv(&value, 1, MPI_INT, 0, 123, MPI_COMM_WORLD, &s);
         value += 2;
         MPI_Send(&value, 1, MPI_INT, 0, 123, MPI_COMM_WORLD);
