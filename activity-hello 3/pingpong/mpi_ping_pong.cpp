fa74c6e86e9a0b8e0cb3ca4843c2fe8c88703f3e
CliftonT <cathorn1@uncc.edu>
2021-12-06 12:53:09 -0500

    init

--- activity-hello 3/pingpong/mpi_ping_pong.cpp
+++ activity-hello 3/pingpong/mpi_ping_pong.cpp
@@ -16,10 +16,10 @@ int main (int argc, char* argv[]) {
     int value = atoi(argv[0]);
     int size;
     int rank;
-    MPI_Status s;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
+    MPI_Status s;
+    
     if (rank == 0) {
         MPI_Send(&value, 1, MPI_INT, 1, 123, MPI_COMM_WORLD);
     }
